c06e5cccc3e6a25dce64f5cac18a647a227df507
shadi-nakhal <lebdroid@gmail.com>
2020-09-28 13:08:22 +0300

    Basic-02 step 1

---
+++ javascript-basics-02/step 1/Step_1.js
@@ -0,0 +1,12 @@
+
+
+var img = document.getElementById("image1");
+
+img.addEventListener("mouseover", function(){
+    img.src = "images/image1_2.jpg"
+});
+
+img.addEventListener("mouseout", function(){
+    img.src = "images/image1.jpg"
+});
+
